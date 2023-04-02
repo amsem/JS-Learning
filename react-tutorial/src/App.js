dc0ff6936d29315f40427740ea009ea3f96e537b
amsem <aminesaidsel@gmail.com>
2023-04-02 11:25:08 +0100

    Work with components and pass diferent types of data

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class Header extends Component {
+  render() {
+    return (
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='App-title'>
+        {this.props.title}
+        </h1>
+        <div>
+        {this.props.num}
+        </div>
+        <div>
+        {JSON.stringify(this.props.myObj)}
+        </div>
+        <div>{this.props.arr[2]}</div>
+        <div>{this.props.Random(9,1)}</div>
       </header>
+    )
+  }
+ }
+
+function App() {
+  const add = (a,b) => a - b;
+  return (
+    <div className="App">
+      <Header title="Hello"
+      num={5} 
+      arr = {[1,2,3]} 
+      Random={add} 
+      myObj={{
+        a: 2,
+        b: 9
+      }} />
     </div>
   );
 }
