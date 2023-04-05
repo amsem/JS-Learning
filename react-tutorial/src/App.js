777e74f94528e39c517a0539608764340cc0015e
amsem <aminesaidsel@gmail.com>
2023-04-05 16:57:34 +0100

    Image Slider using setState

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -3,18 +3,13 @@ import React from 'react';
 import Body from './components/Body';
 import Header from './components/Header';
 import Counter from './components/Counter';
+import ImageSlider from './components/ImageSlider';
 function App() {
   const add = (a,b) => a - b;
   return (
     <div className="App">
-      <Header title="Hello"
-      num={5} 
-      arr = {[1,2,3]} 
-      Random={add} 
-      myObj={{
-        a: 2,
-        b: 9
-      }} />
+      <ImageSlider/>
+     
       <Body text1 = "hello"/>
       <Counter initialCounter={0}/>
       <Counter initialCounter={10}/>
