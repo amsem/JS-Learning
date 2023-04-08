d11e2a0cdd97f235f087efb79face99a0691cfdf
amsem <aminesaidsel@gmail.com>
2023-04-08 17:06:34 +0100

    Optimize and add validation to my form

--- react-tutorial/src/components/Form.js
+++ react-tutorial/src/components/Form.js
@@ -1,40 +1,81 @@
 import React from "react";
 
+ const initialState = {
+    username: "",
+        email:"",
+        password:"",
+        usernameError: "",
+        emailError:"",
+        passwordError:""
+ }
 export default class Form extends React.Component {
     
-    state = {
-        name: "",
-        text: "",
-        checked: false,
-        response: "No"
-    }
+    state = initialState;
     
     handleChange = (event) => {
-        this.setState({name: event.target.value});
+        const checked = event.target.type === "checkbox"
+        this.setState({
+            [event.target.name]: checked
+            ? event.target.checked
+            : event.target.value
+        });
     };
-    handleText = (e) => {
-        this.setState({text: e.target.value});
+    validate = () => {
+        let usernameError= "" ;
+        let emailError= "" ;
+       // let passwordError = "" ;
+        if(!this.state.username){
+            usernameError = "Enter a valid UserName";
+        }
+        if(!this.state.email.includes("@")){
+            emailError = "invalid email !";
+        }
+        if(emailError || usernameError){
+            this.setState({ emailError, usernameError })
+            return false;
+        }
+        return true;
     };
-    handleCheck = (event) => {
-        this.setState({checked: event.target.checked});
-    };
-    handleSelect = (event) => {
-        this.setState({response: event.target.response});
-    };
-    handleSubmit = () => {
+    handleSubmit = (event) =>{
+        event.preventDefault();
+        const isValid = this.validate();
+        if (isValid){
         console.log(this.state);
+        this.setState(initialState);
+        }
+
     };
     render() {
-        return <div className="visible">
-            <input value={this.state.name} onChange={this.handleChange} />
-            <textarea value={this.state.text} onChange={this.handleText} />
-            <input type="checkbox" checked={this.state.checked} onChange={this.handleCheck}/>
-            <select value={this.state.response} onChange={this.handleSelect}>
-                <option>Yes</option>
-                <option>No</option>
-                <option>IDK</option>
-            </select>
-            <button onClick={this.handleSubmit}>SUBMIT</button>
-        </div>
+        return <form className="visible" onSubmit={this.handleSubmit}>
+            <div>
+            <input 
+            placeholder="username"
+            name="username"
+            value={this.state.name} 
+            onChange={this.handleChange} 
+            />
+            <div style={{fontSize: 18, color: "red"}}>{this.state.usernameError}</div>
+            </div>
+            <div>
+            <input 
+            placeholder="email"
+            name="email"
+            value={this.state.name} 
+            onChange={this.handleChange} 
+            />
+             <div style={{fontSize: 18, color: "red"}}>{this.state.emailError}</div>
+            </div>
+            <div>
+            <input 
+            placeholder="password"
+            type="password"
+            name="pasword"
+            value={this.state.name} 
+            onChange={this.handleChange} 
+            />
+             <div style={{fontSize: 18, color: "red"}}>{this.state.passwordError}</div>
+            </div>
+            <button type="submit">submit</button>
+        </form>
     }
 }
