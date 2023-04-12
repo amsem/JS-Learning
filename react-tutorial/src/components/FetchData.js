e24a1f95ee3dc510f5c6443240e54673ebbc3a24
amsem <aminesaidsel@gmail.com>
2023-04-12 22:12:32 +0100

    Rendering Arrays on React

--- react-tutorial/src/components/FetchData.js
+++ react-tutorial/src/components/FetchData.js
@@ -4,26 +4,29 @@ export default class FetchData extends React.Component {
     
     state = {
         loading: true,
-        person: null
+        people: []
     };
     async componentDidMount(){
-        const url = "https://api.randomuser.me/";
+        const url = "https://api.randomuser.me/?results=5";
         const response = await fetch(url);
         const data = await response.json();
-        this.setState({person: data.results[0], loading: false});
+        this.setState({people: data.results, loading: false});
     };
     render(){
         if (this.state.loading){
             return <div>loading</div>
         }
-        if (!this.state.person){
+        if (!this.state.people.length){
             return <div>no data</div>
         }
         return(<div>
-                <div>{this.state.person.name.first}</div>
-                <div>{this.state.person.name.last}</div>
-                <img src={this.state.person.picture.large}/>
-                <div></div>
+            {this.state.people.map(person => (
+                <div key={person.login.uuid}>
+                <div>{person.name.first}</div>
+                <div>{person.name.last}</div>
+                <img src={person.picture.large}/>
+                </div>
+            ))}
                 </div>
                 );
     }
